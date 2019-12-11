f23d413b9fef5da80697a87d4c33c4eb3ab0290c
ElectroluxV2 <electroluxv2@gmail.com>
2019-12-11 23:39:36 +0100

    Version 1.0.2

--- src/app/services/update.service.ts
+++ src/app/services/update.service.ts
@@ -9,7 +9,7 @@ import Swal from 'sweetalert2';
 })
 export class UpdateService {
 
-  protected currentVersion: string = '1.0.1';
+  protected currentVersion: string = '1.0.2';
 
   get version(): String {
     return this.currentVersion;
