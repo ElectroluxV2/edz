b6de6717b6a68d422c13c6c4feb486f35ecfd18a
ElectroluxV2 <electroluxv2@gmail.com>
2019-12-12 22:09:38 +0100

    Fixed white background issue

--- src/app/services/theme.service.ts
+++ src/app/services/theme.service.ts
@@ -21,7 +21,8 @@ export class ThemeService {
     this.overlayContainer.getContainerElement().classList.value = 'cdk-overlay-container ' + name;
 
     // Dirty fix for white background on iOS
-    const rgb = document.body.style.backgroundColor = window.getComputedStyle(document.getElementsByClassName('app-frame')[0]).backgroundColor;
+    const rgb = window.getComputedStyle(document.getElementsByClassName('app-frame')[0]).backgroundColor;
+    document.body.style.backgroundColor = rgb;
     document.documentElement.style.backgroundColor = rgb;
     const hexColor = '#' + rgb.substr(4, rgb.indexOf(')') - 4).split(',').map((color) => String("0" + parseInt(color).toString(16)).slice(-2)).join('');
      
